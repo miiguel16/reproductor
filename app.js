6eecf6ac779699f6e7ee1a13d118c7abb6ea2e5c
Sena <dimatesebas73@gmail.com>
2024-10-21 15:38:54 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const audio = document.getElementById ('audio')
+const play = document.getElementById ('play')
+const pause = document.getElementById ('pause')
+const forward = document.getElementById ('forward')
+const rewind = document.getElementById ('rewind')
+const stop = document.getElementById ('stop')
+
+
+const playlist = {
+    {
+        title:'Daga adicta' ,
+        artist: 'Luigi 21 y J Alvarez' ,
+        img: ,
+        song: 
+    }
+}
+
+play.addEventListener (`click` , () => audio.play)
+
+pause.addEventListener (`click` , () => audio.pause)
+
+rewind.addEventListener (`click` , () => audio.currentTime -= 10)
+
+forward.addEventListener (`click` , () => audio.currentTime += 10)
+
+stop.addEventListener (`click` , () => {
+    audio.pause()
+    audio.currentTime = 0
+})
