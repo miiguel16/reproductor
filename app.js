918dc2011c83a7db80cdf5f1be52b5680874357e
miiguel16 <miguelngelchilito@gmail.com>
2024-10-21 16:10:08 -0500

    add the song and images

--- app.js
+++ app.js
@@ -6,18 +6,39 @@ const rewind = document.getElementById ('rewind')
 const stop = document.getElementById ('stop')
 
 
-const playlist = {
+
+const playList = [
     {
-        title:'Daga adicta' ,
-        artist: 'Luigi 21 y J Alvarez' ,
-        img: ,
-        song: 
-    }
-}              
-
-
-
-
+      title: 'La foto de los 2',
+      artist: 'El andariego',
+      img: './image/andariego.jpg',
+      song: './music/El Andariego - La Foto De Los Dos (Audio Oficial)  Música Popular.mp3'
+    },
+    {
+      title: 'Tuyo',
+      artist: 'Romeo Santos',
+      img: './image/tuyo.jpg',
+      song: './music/Romeo Santos - Tuyo (Audio).mp3'
+    },
+    {
+      title: 'Así no',
+      artist: 'Sono Lux',
+      img: './image/asi_no.jpg',
+      song: './music/Sono-Lux - Asi No.mp3'
+    },
+    {
+      title: 'Soltera',
+      artist: 'Blessd y Westcol',
+      img: './image/soltera.jpg',
+      song: './music/W Sound 01 Soltera - Blessd, Westcol, Ovy On The Drums.mp3'
+    },
+    {
+      title: 'Las 12',
+      artist: 'Kevin Roldan',
+      img: './image/las_12.jpg',
+      song: './music/Kevin Roldan ft Mackie - Las 12 (Official Audio).mp3'
+    },
+  ]            
 
 
 play.addEventListener (`click` , () => audio.play)
